5b28091e46ddd186852b11c99bcff6ce04737724
ghettoripper <ap4ixba@gmail.com>
2024-03-08 14:13:21 +0100

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,16 @@
 const loginForm = document.querySelector(".login-form");
-function  handleLogin(event) {
-    event.preventDefault();
-    const form = event.target;
-    const  email = form.elements.email.value;
-    const password = form.elements.password.value;
-    if (!email ||  !password) {
-        alert("All form fields must be filled in!");
-    }
-    else {
-        console.log(`Email: ${email}, Password: ${password}`);
-        form.reset();
-    }
+function handleLogin(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  if (!email || !password) {
+    alert("All form fields must be filled in!");
+  } else {
+    const userData = { email: email, password: password };
+    console.log("User data ", userData);
+    form.reset();
+  }
 }
 
-
-loginForm.addEventListener("submit", handleLogin)
-
+loginForm.addEventListener("submit", handleLogin);
